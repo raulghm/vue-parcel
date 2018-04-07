c9b65a4359e6c1c020c03147670dd508d0718a1a
raulghm <raulghm@gmail.com>
2018-04-07 12:24:50 -0300

    feat(): store

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import router from './router'
+import store from './store'
 import App from './components/App'
 
 new Vue({
   el: '#root',
   router,
+  store,
   template: '<App/>',
   components: { App },
   render: h => h(App),
